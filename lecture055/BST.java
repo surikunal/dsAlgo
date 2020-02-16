ab981f05739ac927e72b94a972c8ace71646b9e0
kunal <suri.kunal.222@gmail.com>
2020-02-16 23:26:31 +0530

    BST

--- lecture055/BST.java
+++ lecture055/BST.java
@@ -1,28 +1,5 @@
 public class BST {
-    // public static Scanner scn = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        BST();
-    }
-    public static void BST() {
-        int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-        Node root = constructBST(arr, 0, 9);
-        // display(root);
-        // System.out.println(height(root));
-        // System.out.println(size(root));
-        // System.out.println(find_01(root, 40));      //recursively
-        // System.out.println(find_02(root, 300));      //iteratively
-        // addDate(root, 200);
-        // System.out.println(maxInTree(root));
-        // System.out.println(minInTree(root));
-        // deleteNode(root, 50);
-        // display(root);
-        /* moris traversal for inOder and preOrder */
-        morisInOrder(root);
-        System.out.println();
-        morisPreOrder(root);
-    }
-
+    
     static class Node {
         int data = 0;
         Node left = null;
@@ -60,58 +37,6 @@ public class BST {
         display(node.right); // display(2*idx+2);
     }
 
-    public static boolean find_01(Node node, int data) {
-        if (node == null) {
-            return false;
-        }
-        if (node.data == data) {
-            return true;
-        } else if (data < node.data) {
-            return find_01(node.left, data);
-        } else
-            return find_01(node.right, data);
-    }
-
-    public static int size(Node node) {
-        if (node == null)
-            return 0;
-        return size(node.left) + size(node.right) + 1;
-
-    }
-
-    public static int height(Node node) {
-        if (node == null)
-            return -1;
-        return Math.max(height(node.left), height(node.right)) + 1;
-    }
-
-    public static Node addDate(Node node, int data) {
-        if (node == null) {
-            return new Node(data);
-        }
-
-        if (data < node.data)
-            node.left = addDate(node.left, data);
-
-        else
-            node.right = addDate(node.right, data);
-
-        return node;
-    }
-
-    public static boolean find_02(Node node, int data) {
-        while (node != null) {
-            
-            if (node.data == data) {
-                return true;
-            } else if (data < node.data) {
-                return find_02(node.left, data);
-            } else
-                return find_02(node.right, data);
-        }
-        return false;
-    }
-
     public static int maxInTree(Node node) {
         if (node == null) {
             return Integer.MIN_VALUE;
@@ -145,12 +70,12 @@ public class BST {
 
             int max = maxInTree(node.left);
             node.data = max;
-            node.left = deleteNode(node.left, data);
+            node.left = deleteNode(node.left, max);
 
         } else if (data < node.data) {
-            deleteNode(node.left, data);
+            node.left = deleteNode(node.left, data);
         } else
-            deleteNode(node.right, data);
+            node.right = deleteNode(node.right, data);
 
         return node;
     }
@@ -206,6 +131,24 @@ public class BST {
                 }
             }
         }
+    }
+
+    public static void BST_() {
+        int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        Node root = constructBST(arr, 0, 9);
+        // display(root);
+
+        // System.out.println(maxInTree(root));
+        // System.out.println(minInTree(root));
+        // deleteNode(root, 50);
+        // display(root);
+        /* moris traversal for inOder and preOrder */
+        morisInOrder(root);
+        System.out.println();
+        morisPreOrder(root);
+    }
+    public static void main(String[] args) {
+        BST_();
     }
 }
 
