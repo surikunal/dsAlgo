585b98b65052c89ef9cc9f9e415c92758267740c
kunal <suri.kunal.222@gmail.com>
2020-02-19 23:04:03 +0530

    AVL

--- lecture056/AVL.java
+++ lecture056/AVL.java
@@ -1,32 +1,6 @@
 public class AVL {
-    // public static Scanner scn = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        AVL_();
-    }
-
-    public static void AVL_() {
-        int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-        Node root = constructBST(arr, 0, 9);
-        // display(root);
-        // System.out.println(height(root));
-        // System.out.println(size(root));
-        // System.out.println(find_01(root, 40)); //recursively
-        // System.out.println(find_02(root, 300)); //iteratively
-        // addDate(root, 200);
-        // display(root);
-        // System.out.println();
-        // System.out.println(maxInTree(root));
-        // System.out.println(minInTree(root));
-        // deleteNode(root, 200);
-        // display(root);
-        // display(root);
-
-        System.out.println(rotation(root).data);
-
-    }
-
-    static class Node {
+    
+    public static class Node {
         int data = 0;
         int bal = 0;
         int height = -1;
@@ -42,6 +16,24 @@ public class AVL {
             this.right = right;
             this.data = data;
         }
+
+        @Override
+        public String toString() {
+            String str = "";
+            str += ((left != null ? left.data : " . "));
+            str += (" -> " + data + " <- ");
+            str += ((right != null ? right.data : " . "));
+            str += "\n";
+
+            if (left != null) {
+                str += left.toString();
+            }
+
+            if (right != null) {
+                str += right.toString();    
+            }
+            return str;
+        }
     }
 
     public static Node constructBST(int[] arr, int si, int ei) {
@@ -55,55 +47,45 @@ public class AVL {
         return node;
     }
 
-    public static void display(Node node) {
-        if (node == null)
-            return;
-        System.out.print((node.left != null ? node.left.data : "."));
-        System.out.print(" -> " + node.data + " <- ");
-        System.out.println((node.right != null ? node.right.data : "."));
-        display(node.left); // display(2*idx+1);
-        display(node.right); // display(2*idx+2);
-    }
+    // public static void display(Node node) {
+    //     if (node == null)
+    //         return;
+    //     System.out.print((node.left != null ? node.left.data : "."));
+    //     System.out.print(" -> " + node.data + " <- ");
+    //     System.out.println((node.right != null ? node.right.data : "."));
+    //     display(node.left); // display(2*idx+1);
+    //     display(node.right); // display(2*idx+2);
+    // }
 
-    public static int getHeight(Node node) {
+    public static void display(Node node) {
         if (node == null) {
-            return -1;
+            return;
         }
 
-        int lh = -1;
+        String str = "";
+
+        str += node.left == null ? " . " : node.left.data;
+        str += " -> " + node.data + " <- ";
+        str += node.right == null ? " . " : node.right.data;
+
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void updateHeightAndBalanceFactor(Node node) {
         int rh = -1;
+        int lh = -1;
 
         if (node.left != null) {
             lh = node.left.height;
         }
-        if (node.right != null) {
-            rh = node.right.height;
-        }
-
-        return Math.max(lh, rh) + 1;
-    }
-
-    public static int balanceFactor(Node node) {
-        if (node == null) {
-            return 0;
-        }
-
-        int rh = -1;
-        int lh = -1;
-
-        if (node.left != null) {
-            lh = node.right.height;
-        }
 
         if (node.right != null) {
             rh = node.right.height;
         }
-        return (lh - rh);
-    }
 
-    public static void getFactors(Node node) {
-        node.height = getHeight(node);
-        node.bal = balanceFactor(node);
+        node.height = Math.max(lh, rh) + 1;
+        node.bal = (lh - rh);
     }
 
     public static Node leftRotation(Node x) { // ll
@@ -112,6 +94,9 @@ public class AVL {
 
         y.right = x;
         x.left = yright;
+
+        updateHeightAndBalanceFactor(x);
+        updateHeightAndBalanceFactor(y);
 
         return y;
     }
@@ -123,29 +108,32 @@ public class AVL {
         y.left = x;
         x.right = yleft;
 
+        updateHeightAndBalanceFactor(x);
+        updateHeightAndBalanceFactor(y);
+
         return y;
     }
 
     public static Node rotation(Node node) {
-        //    getFactors(node);
-        if (balanceFactor(node) == 2) {
-            if (balanceFactor(node.left) == 1) {
-                node = rightRotation(node);
+        updateHeightAndBalanceFactor(node);
+        if (node.bal == 2) {
+            if (node.left.bal == 1) {
+                return rightRotation(node);
             }
-            else {
+            else if (node.left.bal == -1) {
                 node.left = leftRotation(node.left);
-                node = rightRotation(node);
+                return rightRotation(node);
             }
         }
 
         else if (node.bal == -2) {
-            if (balanceFactor(node.right) == -1) {
-                node = leftRotation(node);
+            if (node.right.bal == -1) {
+                return leftRotation(node);
             }
 
-            else {
+            else if (node.right.bal == 1) {
                 node.right = rightRotation(node.right);
-                node = leftRotation(node);
+                return leftRotation(node);
             }
         }
         return node;
@@ -184,7 +172,7 @@ public class AVL {
         else
             node.right = addDate(node.right, data);
 
-        getFactors(node); // first update everything to correct
+         // update everything to correct
         node = rotation(node); // call rotation before returning
         return node;
     }
@@ -193,6 +181,7 @@ public class AVL {
         if (node == null) {
             return null;
         }
+
         if (node.data == data) {
             if (node.left == null || node.right == null) {
                 return node.left == null ? node.right : node.left;
@@ -201,14 +190,41 @@ public class AVL {
             int max = maxInTree(node.left);
             node.data = max;
             node.left = deleteNode(node.left, max);
-
-        } else if (data < node.data) {
+        }
+        
+        else if (data < node.data) {
             node.left = deleteNode(node.left, data);
-        } else
+        }
+        
+        else
             node.right = deleteNode(node.right, data);
 
-        getFactors(node); // first update everything to correct
+        // first update everything to correct
         node = rotation(node); // call rotation before returning
         return node;
+    }
+
+    public static void AVL_() {
+        int[] arr = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        Node root = constructBST(arr, 0, arr.length - 1);
+        display(root);
+        // System.out.println(height(root));
+        // System.out.println(size(root));
+        // System.out.println(find_01(root, 40)); //recursively
+        // System.out.println(find_02(root, 300)); //iteratively
+        addDate(root, 200);
+        display(root);
+        System.out.println();
+        // System.out.println(maxInTree(root));
+        // System.out.println(minInTree(root));
+        deleteNode(root, 200);
+        display(root);
+        // display(root);
+
+        // System.out.println(rotation(root).data);
+    }
+
+    public static void main(String[] args) {
+        AVL_();
     }
 }
