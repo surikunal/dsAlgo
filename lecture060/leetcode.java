a8bf490e4e43b4362dbef0e526c239a520b74945
kunal <suri.kunal.222@gmail.com>
2020-02-26 20:20:21 +0530

    leetcode 735,402

--- lecture060/leetcode.java
+++ lecture060/leetcode.java
@@ -1,9 +1,12 @@
-/* leet code question 735 */
+
+/* leet code question 735, 402 */
 import java.util.Stack;
+
 public class leetcode {
     public static void main(String[] args) {
         int[] arr = { 5, 10, -5 };
         astroidCollision_735(arr);
+        removeKdigits_402();
     }
 
     public static int[] astroidCollision_735(int[] arr) {
@@ -28,5 +31,40 @@ public class leetcode {
             ans[i] = st.pop();
         }
         return ans;
+    }
+
+    public String removeKdigits(String num, int k) {
+        char[] arr = num.toCharArray();
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            while (st.size() != 0 && st.peek() > arr[i]) {
+                st.pop();
+                k--;
+            }
+            st.push(arr[i]);
+        }
+
+        while (k-- > 0) {
+            st.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (st.size() != 0) {
+            sb.append(st.pop());
+        }
+
+        if (sb.length() != 0 && sb.charAt(sb.length() - 1) == '0') {
+            int i = sb.length() - 2;
+            for (; i >= 0; i--) {
+                if (sb.charAt(i) != 0) {
+                    break;
+                }
+            }
+            sb = new StringBuilder(sb.substring(0, i + 1));
+        }
+        sb.reverse();
+        String ans = sb.toString();
+        return ans.length() != 0 ? ans : "0";
     }
 }
