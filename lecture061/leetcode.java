6ab9a2bb2f3edcc9a882627a260ba6c2898bd145
kunal <suri.kunal.222@gmail.com>
2020-02-28 23:54:02 +0530

    largestRectangleArea

--- lecture061/leetcode.java
+++ lecture061/leetcode.java
@@ -1,5 +1,39 @@
+import java.util.Stack;
 public class leetcode {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,4,6,6,6,5,2,4,6,5};
+        System.out.println(largestRectangleArea(arr));
+    }
+
+    public static int largestRectangleArea(int[] arr) {
+        int maxArea = 0;
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
         
+        for (int i = 0; i < arr.length; i++) {
+            while (st.peek() != -1 && arr[i] <= arr[st.peek()]){
+                int height = arr[st.pop()];
+                int width = (i - st.peek() - 1);
+                int area = height * width;
+                
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
+            
+            st.push(i);
+        }
+        
+        while (st.peek() != -1) {
+            int height = arr[st.pop()];
+            int width = (arr.length - st.peek() - 1);
+            int area = height * width;
+            
+            if (area > maxArea) {
+                maxArea = area;
+            }
+        }
+        
+        return maxArea;
     }
 }
