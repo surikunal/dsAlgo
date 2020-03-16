0c65a256bae94ca55368d4f6a6f58822d1ef7368
kunal <suri.kunal.222@gmail.com>
2020-03-16 23:29:37 +0530

    leetcode

---
+++ lecture062/leetcode.java
@@ -0,0 +1,28 @@
+class MinStack {
+    public MinStack(int data) {
+        this.data = data;
+    }
+    
+    public void push(int x) {
+        
+    }
+    
+    public void pop() {
+        
+    }
+    
+    public int top() {
+        
+    }
+    
+    public int getMin() {
+        
+    }
+}
+
+public class leetcode {
+    public static void main(String[] args) {
+        minStack_155();
+    }
+
+}
