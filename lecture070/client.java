c113cd1f9103162581bd4b39a39ba3428fd70716
kunal <suri.kunal.222@gmail.com>
2020-04-04 11:29:43 +0530

    hashmap

---
+++ lecture070/client.java
@@ -0,0 +1,20 @@
+public class client {
+    public static void main(String[] args) {
+        hashMap_<String, Integer> map = new hashMap_(); // ! here we can take anything in btw <> brackets
+        map.put("csad", 100);
+        map.put("Asd", 110);
+        map.put("asd", 140);
+        map.put("123", 140);
+        map.put("566", 140);
+        System.out.println(map);
+
+        // map.put(10, 100);
+        // map.put(20, 200);
+        // map.put(30, 300);
+        // map.remove(10);
+        // System.out.println(map.get(20));
+        // System.out.println(map);
+        // System.out.println(map.getOrDefault(40, -1));
+        // System.out.println(map.keySet());
+    }
+}
