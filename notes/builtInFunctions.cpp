5a226680a6e8371fc65ad2b8b73f3343f423242b
kunal <suri.kunal.222@gmail.com>
2020-04-30 21:27:58 +0530

    update

---
+++ notes/builtInFunctions.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+cout << __builtin_popcount(7) << endl;
+cout << __builtin_popcount(13) << endl;
+//* built in function to count number of set bits (number of 1's);
