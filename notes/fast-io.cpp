a244e6bb2cc631caa14a141dcae7bf54f1398bb1
kunal <suri.kunal.222@gmail.com>
2020-05-08 01:42:57 +0530

    fast input output

---
+++ notes/fast-io.cpp
@@ -0,0 +1,9 @@
+auto SpeedUp = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+// to make a code even faster
+// this will make the input and output functions a little faster
---
+++ notes/tryCode.java
@@ -0,0 +1,13 @@
+
+//* for fast input output 
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class tryCode {
+    public static Scanner scn = new Scanner(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        int n = scn.nextInt();
+        System.out.println(n);
+    }
+}
